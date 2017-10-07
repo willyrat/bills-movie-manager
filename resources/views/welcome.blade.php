067b907bae8768be081cbf845ebb70f8a59e1f7b
William Pratt <willyrat@gmail.com>
2017-10-06 20:34:38 -0400

    just testing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,9 @@
                 <div class="title m-b-md">
                     Laravel
                 </div>
-
+                <dev>
+                    test it out
+                </dev>
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
